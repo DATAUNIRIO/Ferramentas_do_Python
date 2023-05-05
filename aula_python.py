fed7cfb759eaa6be158353caf2495f94b7989ba0
DATAUNIRIO <duttross@gmail.com>
2023-05-05 14:51:05 -0300

    aula

---
+++ aula_python.py
@@ -0,0 +1,11 @@
+
+
+import pandas as pd
+import numpy as np
+
+QE = pd.read_csv("https://raw.githubusercontent.com/DATAUNIRIO/Base_de_dados/master/QE.csv") 
+
+print(carro.head(3))
+# listar os objetos
+%whos
+df['col_name'] = df['col_name'].astype('category')
